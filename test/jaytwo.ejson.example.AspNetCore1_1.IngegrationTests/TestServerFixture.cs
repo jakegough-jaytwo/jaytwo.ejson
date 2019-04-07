bbd5e61e0425d2246c554327f03db74803f03026
Jake Gough <jakegough@gmail.com>
2019-04-06 23:35:07 -0600

    using jaytwo.SolutionResolution in integration tests

--- test/jaytwo.ejson.example.AspNetCore1_1.IngegrationTests/TestServerFixture.cs
+++ test/jaytwo.ejson.example.AspNetCore1_1.IngegrationTests/TestServerFixture.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using jaytwo.SolutionResolution;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 
@@ -20,32 +21,12 @@ namespace jaytwo.ejson.example.AspNetCore1_1.IngegrationTests
                 variable: "EJK_3d953564513b09af30c9c9724c52770a2ffd13862710de857f5ef75e69350e52",
                 value: "edadd0dc3f1765d78122f752ca5c01292916cba2e7e09fe796f5dcc2423faadd");
 
-            var solutionRelativePath = GetSolutaionRelativePath("examples/jaytwo.ejson.example.AspNetCore1_1");
+            var contentRoot = new SlnFileResolver().ResolvePathRelativeToSln("examples/jaytwo.ejson.example.AspNetCore1_1");
 
             _server = new TestServer(new WebHostBuilder()
                 .UseEnvironment("Development")
-                .UseContentRoot(solutionRelativePath)
+                .UseContentRoot(contentRoot)
                 .UseStartup<Startup>());
-        }
-
-        public string GetSolutaionRelativePath(string relativePath)
-        {
-            var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
-            var slnPresent = false;
-
-            while (!slnPresent && directory.Parent != null)
-            {
-                directory = directory.Parent;
-                slnPresent = directory.GetFiles("*.sln", SearchOption.TopDirectoryOnly).Any();
-            }
-
-            if (slnPresent)
-            {
-                var path = Path.Combine(directory.FullName, relativePath);
-                return new DirectoryInfo(path).FullName;
-            }
-
-            throw new InvalidOperationException("Could not find solution file!");
         }
 
         public void Dispose()
